ddcfdb88c3e1e77402c7612dcc1eddac15225f52
rnagulapalle <rnagulapalle@lm-sjn-00876073.paypalcorp.com>
2015-07-06 21:08:32 -0700

    init commit

---
+++ src/main/java/com/ufc/bootstrapper/mvcconfig/MvcConfiguration.java
@@ -0,0 +1,29 @@
+package com.ufc.bootstrapper.mvcconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: will
+ * Date: 11/23/13
+ * Time: 12:27 PM
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.ufc.bootstrapper.controllers"})
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
